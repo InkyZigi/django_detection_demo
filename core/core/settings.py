382224b163933b76d9c7be103d2cec164a353889
Zigi <1635987936@qq.com>
2024-07-04 19:36:00 +0800

    20240704

--- core/core/settings.py
+++ core/core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&=ok&egtaecpl#$hvw#k^^%)o%976gpk#y#!4!bw7hki1_h9#s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
